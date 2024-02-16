ed7a032840204e9034a89412997ef8fce9196ce1
itschool0089 <155607919+Yotaro-Yamamoto@users.noreply.github.com>
2024-02-16 04:15:29 +0000

    Create 2-3.ts

---
+++ TypeScript/Adv/C2/ts/2-3.ts
@@ -0,0 +1,12 @@
+//オブジェクトの型注釈
+const obj:{
+    name: string;
+    flavor: string;
+} = {
+    name:"缶ジュース",
+    flavor:"りんご味"
+};
+console.log(obj);
+//以下はプリミティブ型の型注釈
+//const flavor:string = "りんご味";
+
