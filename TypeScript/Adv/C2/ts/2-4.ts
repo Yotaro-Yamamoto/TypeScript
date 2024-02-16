91d9082698576c57cd7125dc48a3447fa6fee3ec
itschool0089 <155607919+Yotaro-Yamamoto@users.noreply.github.com>
2024-02-16 04:30:55 +0000

    Update 2-4.ts

--- TypeScript/Adv/C2/ts/2-4.ts
+++ TypeScript/Adv/C2/ts/2-4.ts
@@ -1,5 +1,5 @@
 //読み取り専用プロパティ
-//readonly修飾子
+//書式1 readonly修飾子
 type Juice = {
     readonly name:string;   //読み取り専用のプロパティ
     flavor:string;
@@ -14,3 +14,16 @@ console.log(obj);
 //obj:name = "ペットボトル" ←コンパイルエラー
 obj.flavor = "メロン味";    //代入可能
 console.log(obj);
+
+//書式2 constアサーション
+/*オブジェクトリテラルの末尾にas constをつけると
+  全てのプロパティが、読みより専用になる。*/
+
+const obj2 = {
+    name:"缶ジュース",
+    flavor:"りんご味",
+}as const;
+//プロパティの値を書き換えようとすると、コンパイルエラーとなる。
+//obj2.name="ペットボトル" ←コンパイルエラー
+//obj2.flavor="メロン味"  ←コンパイルエラー
+console.log(obj2);
