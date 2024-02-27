a61ea63e274c45dd23e40db2d08314dfdf567677
itschool0089 <155607919+Yotaro-Yamamoto@users.noreply.github.com>
2024-02-27 03:50:47 +0000

    Update 3-14.ts

--- TypeScript/Adv/C3/ts/3-14.ts
+++ TypeScript/Adv/C3/ts/3-14.ts
@@ -7,19 +7,31 @@
 　インスタンスを生成しなくてもクラス名をつけるだけで参照できる。逆に言うと、
 　インスタンスからは参照できない。*/
 class Drink{
-    static isDrink(obj:Drink):boolean{
+    /*static isDrink(obj:Drink):boolean{
         return obj = Drink;//objが飲み物ならtrueを返す処理;
-    }
+    }*/
     //静的メソッドの使い道
     /*インスタンスを生成して返す処理を静的メソッドにすると、コンストラクタを使わずに
     　インスタンスを得ることができる。*/
-    static create(name:Drink):Drink{
+    static create(name:string):Drink{
         return new Drink(name);
     }
 }
 const coffee = Drink.create("コーヒー");//newする必要なし
 console.log(coffee);
 
-const curry = new Drink("カレー");
-console.log(Drink.isDrink(curry));//->false
+//const curry = new Drink("カレー");
+//console.log(Drink.isDrink(curry));//->false
 //console.log(curry.isDrink(curry));//コンパイルエラー
+
+//静的メソッドのメリット
+/*・インスタンスを生成せずに呼び出すことができる
+　・インスタンスごとに動作が変わらない
+　・与えた引数だけでメソッドの挙動が決まる(インスタンスの状態に依存しない)*/
+
+//静的メソッドの制約
+/*静的メソッドは特定のインスタンスに備わっているわけではないので、静的メソッドの内部で
+　インスタンスのプロパティやインスタンスのメソッドを参照するとコンパイルエラーになる。
+static create(name:string):Drink{
+    return new Drink(this._name);//コンパイルエラー
+}*/
