a11d024a78e5ae01f0e8cb3be9a1538b316803af
Yotaro Yamamoto <155607919+Yotaro-Yamamoto@users.noreply.github.com>
2024-02-29 14:18:55 +0900

    Update 3-22.ts

--- TypeScript/Adv/C3/ts/3-22.ts
+++ TypeScript/Adv/C3/ts/3-22.ts
@@ -16,3 +16,22 @@ abstract class Drink{
     constructor(){}
     abstract showAmount():void;//分量を出力するメソッド
 }
+
+//具象クラス
+class Coffee extends Drink{
+    protected _aomunt: number;//分量(ml)
+    protected _sugar:number//砂糖の量(g)
+    constructor(amount:number,sugar:number){
+        super();//このコンストラクタは何もしない
+        this._aomunt = amount;
+        this._sugar = sugar;
+    }
+    showAmount(): void {
+        console.log(this._aomunt + this._sugar);
+    }
+}
+/*このとき、具象クラスのコンストラクタでsuper()を最初に実行し、その後で、プロパティの
+　初期化を行う必要がある。super()が呼び出すDrinkクラスのコンストラクタは具体的な初期
+　化処理を行わないが、文法上の制約により必ず呼び出さなければならない。*/
+const moca = new Coffee(100,5);
+console.log(moca);
