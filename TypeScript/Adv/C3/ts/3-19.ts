caccbc0138d5769a18736f2c6ce54b3008e4d438
Yotaro Yamamoto <155607919+Yotaro-Yamamoto@users.noreply.github.com>
2024-02-29 13:12:09 +0900

    Update 3-19.ts

--- TypeScript/Adv/C3/ts/3-19.ts
+++ TypeScript/Adv/C3/ts/3-19.ts
@@ -7,15 +7,12 @@ class Drink{
     protected _price:number;
     getPrice():number{return this._price}
 }
-/*たとえばDrinkクラスのgetPriceメソッドが返す価格が税別に変わったとしても、SaleDrinkクラスの
-　getPriceメソッドは常にその半額を返す。つまり、スーパークラスの機能を利用しつつ、サブクラス独自の
-　処理を加えた形になる。
-　目的に合わせてスーパークラスとサブクラスのメソッドを使い分けできた方が良い場合は、オーバーライド
-　しなくても構わない
-　次のコードは、元の価格を取得したいときhgetPriceメソッド、半額の価格を取得したいときはgetHalf
-  Priceメソッドを使い分けできるようにサブクラスを実装する。*/
+
 class SaleDrink extends Drink{
     getPrice():number{//半額で提供
-        return Math.floor(super.getPrice()/2);
+        return Math.floor(super.getPrice() / 2);
     }
 }
+
+const drink = new Drink();
+console.log(drink);
