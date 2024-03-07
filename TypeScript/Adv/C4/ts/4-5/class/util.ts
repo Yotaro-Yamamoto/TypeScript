9352a1bcf8686d740c51f4396caa307c5a29bdbe
itschool0089 <155607919+Yotaro-Yamamoto@users.noreply.github.com>
2024-03-07 04:45:22 +0000

    Create util.ts

---
+++ TypeScript/Adv/C4/ts/4-5/class/util.ts
@@ -0,0 +1,6 @@
+//関数1
+export const func1 = ():void =>{console.log("関数1");}
+//関数2
+export const func2 = ():void => {console.log("関数2")}
+//関数3
+export const func3 = ():void => {console.log("関数3");}
